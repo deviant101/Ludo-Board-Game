7f3cf485e9d0c0abbde94f95a97e0c34cb145733
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-05-04 13:13:36 +0500

    Added Playground Grids for 1-Player only

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 using namespace std;
 
+    vector<int> y1_home = {4,5,7,10,13,16};
+    vector<int> y2_home = {1,4,7,10,13,};
+    vector<int> x1_home = {1,7,8,9,12,13};
+    vector<int> x2_home = {1,4,7,10,13};
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(750, 750), "Ludo-Board Game");
 
@@ -29,6 +34,27 @@ int main() {
     sf::RectangleShape insideCenter(sf::Vector2f(100, 100));
     insideCenter.setFillColor(sf::Color::Magenta);
     insideCenter.setPosition(325, 325);
+
+    // Create the squares
+    sf::RectangleShape squares[18];
+    for(int i = 0; i < 6; ++i) {
+        for(int j = 0; j < 3; ++j) {
+            int index = i * 3 + j;
+            squares[index].setSize(sf::Vector2f(50, 50));
+            
+            if(find(y1_home.begin(), y1_home.end(), index) != y1_home.end()){
+                squares[index].setFillColor(sf::Color::Green);
+            }else{
+                squares[index].setFillColor(sf::Color::White);
+            }
+
+            // squares[index].setFillColor(sf::Color::White);
+            squares[index].setPosition(300 + (50 * j), 50 * i);
+            squares[index].setOutlineThickness(2);
+            squares[index].setOutlineColor(sf::Color::Black);
+            window.draw(squares[index]);
+        }
+    }
 
     while (window.isOpen()) {
         sf::Event event;
@@ -59,8 +85,13 @@ int main() {
         window.draw(square2);
         window.draw(square3);
         window.draw(square4);
+
         window.draw(center);
         window.draw(insideCenter);
+
+        for(int i = 0; i < 18; ++i) {
+            window.draw(squares[i]);
+        }
 
         window.display();
     }
